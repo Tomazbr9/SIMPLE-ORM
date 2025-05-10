0bf6ac71c1c48a8c0d1530ddd882243218894499
Bruno Tomaz <tomazcraft@gmail.com>
2025-05-10 02:26:10 -0300

    Foi criado a logica para a criação da conexão de um determinado banco de dados

---
+++ orm/engine.py
@@ -0,0 +1,53 @@
+from urllib.parse import urlparse
+
+from exceptions import InvalidUrlError
+
+import re
+
+def disassemble_url(url):
+    regex = '^[a-z]+://[a-z]+:[a-z0-9]+@[a-z0-9.]+:[1-9][0-9]*/[a-z]+$'
+
+    if re.search(regex, url):
+        url_demount = urlparse(url)
+        return url_demount
+    else:
+        raise InvalidUrlError('URL inválida para o padrão esperado')
+
+def check_connection_type(url_demount):
+
+    connections = {
+        'mysql': lambda : create_connection_mysql(url_demount),
+        'postgree': lambda : create_connection_postgree(url_demount),
+        'sqlite': lambda : create_connection_sqlite()
+    }
+
+    return connections[url_demount.scheme]()
+
+def create_connection_mysql(connection_data):
+    import mysql.connector
+
+    connection = mysql.connector.connect(
+        host=connection_data.host,
+        user=connection_data.username,
+        password=connection_data.password,
+        database=connection_data.path.lstrip('/')
+    )
+
+    cursor = connection.cursor()
+
+def create_connection_sqlite():
+    ...
+
+def create_connection_postgree(connection_data):
+    ...
+
+def create_engine(url):
+
+    url_demount = disassemble_url(url)
+    connection = check_connection_type(url_demount)
+    
+    return connection
+
+
+if __name__ == '__main__':
+     disassemble_url('mysql://bruno:12345@localhost:3366/banco')
