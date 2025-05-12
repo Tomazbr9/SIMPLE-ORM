7616bd2ee2f7dc3cb0ef57bac9286a3a8b952ffd
Bruno Tomaz <tomazcraft@gmail.com>
2025-05-12 02:29:30 -0300

    Finalizado todas as conexões

--- orm/engine.py
+++ orm/engine.py
@@ -5,9 +5,9 @@ from exceptions import InvalidUrlError
 import re
 
 def disassemble_url(url):
-    regex = '^[a-z]+://[a-z]+:[a-z0-9]+@[a-z0-9.]+:[1-9][0-9]*/[a-z]+$'
-
-    if re.search(regex, url):
+    regex_mysql_postgree = '^[a-z]+://[a-z]+:[a-z0-9]+@[a-z0-9.]+:[1-9][0-9]*/[a-z]+$'
+    regex_sqlite = '^sqlite:///[a-z]+.db$'
+    if re.search(regex_mysql_postgree, url) or re.search(regex_sqlite, url):
         url_demount = urlparse(url)
         return url_demount
     else:
@@ -18,7 +18,7 @@ def check_connection_type(url_demount):
     connections = {
         'mysql': lambda : create_connection_mysql(url_demount),
         'postgree': lambda : create_connection_postgree(url_demount),
-        'sqlite': lambda : create_connection_sqlite()
+        'sqlite': lambda : create_connection_sqlite(url_demount)
     }
 
     return connections[url_demount.scheme]()
@@ -33,13 +33,27 @@ def create_connection_mysql(connection_data):
         database=connection_data.path.lstrip('/')
     )
 
-    cursor = connection.cursor()
+    return connection.cursor()
 
-def create_connection_sqlite():
-    ...
+def create_connection_sqlite(connection_data):
+    import sqlite3
+
+    connection = sqlite3.connect(connection_data.path)
+
+    return connection.cursor()
 
 def create_connection_postgree(connection_data):
-    ...
+    import psycopg2
+
+    connection = psycopg2.connect(
+        host=connection_data.host,
+        user=connection_data.username,
+        password=connection_data.password,
+        database=connection_data.path.lstrip('/'),
+        port=connection_data.port
+    )
+
+    return connection.cursor()
 
 def create_engine(url):
 
@@ -47,7 +61,6 @@ def create_engine(url):
     connection = check_connection_type(url_demount)
     
     return connection
-
 
 if __name__ == '__main__':
      disassemble_url('mysql://bruno:12345@localhost:3366/banco')
